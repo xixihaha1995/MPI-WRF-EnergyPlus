a38f09ebdd2f1c141a7d6ab9490c1d379800310a
JasonWok@LinuxMint <wulicheneason@gmail.com>
2023-04-23 14:56:39 -0600

    global read me

--- energyplus_c/_test1.c
+++
@@ -1,8 +0,0 @@
-#include <EnergyPlus/api/state.h>
-#include <stdio.h>
-int main(int argc, const char * argv[]) {
-        EnergyPlusState state = stateNew();
-        stateReset(state);
-        stateDelete(state);
-        printf("simple ep test with c.\n");
-}
