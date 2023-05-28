acbf69fcdc3053418bb782ace4a32e63142eeb36
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-05-28 16:46:42 -0600

    try on linuxmint

---
+++ _8_beartooth/cMPIThread/mpi_thread.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <pthread.h>
+
+void* print_pid_tid(void* arg) {
+    pid_t pid = getpid();
+    pthread_t tid = pthread_self();
+    printf("Process %d: Thread PID: %d, TID: %lu\n", *(int*)arg, pid, tid);
+    return NULL;
+}
+
+int main(int argc, char** argv) {
+    int rank, provided;
+    
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("Process %d: Provided level of thread support: %d\n", rank, provided);
+    
+    pthread_t thread;
+    pthread_create(&thread, NULL, print_pid_tid, &rank);
+    pthread_join(thread, NULL);
+    
+    MPI_Finalize();
+    
+    return 0;
+}
