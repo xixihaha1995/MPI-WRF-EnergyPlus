4b2f3b62223dac10dcbd5f4623a84691a8cc8bb5
linuxmint@optiplex-7080 <wulicheneason@gmail.com>
2023-05-28 17:02:43 -0600

    try it on HPC

---
+++ _8_beartooth/cMPIThread/mpithread.slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=communitybem
+#SBATCH --time=00:01:00
+#SBATCH --ntasks=3
+#SBATCH --job-name=test_lw
+#SBATCH --output=test_lw%j.out
+#SBATCH --error=test_lw%j.err
+
+# Set the directory where you want to install the software, Change to the source code directory
+DIR="/home/lwu4/fortran_experiments/_8_beartooth/cMPIThread"
+cd $DIR
+pwd
+make
+echo "Running on $(hostname)"
+# Execute the program
+mpirun  mpi_thread_funneled_example
