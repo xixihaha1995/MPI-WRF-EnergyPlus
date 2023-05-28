173863f59c152a74fc91364f28620f562ac1d6b0
Lichen Wu <lwu4@blog2.cluster>
2023-05-28 17:28:06 -0600

    accept updates

--- _8_beartooth/cMPIThread/mpi_thread_funneled_example.c
+++ _8_beartooth/cMPIThread/mpi_thread_funneled_example.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <unistd.h>
 #include <pthread.h>
 
 void* print_pid_tid(void* arg) {
     pid_t pid = getpid();
     pthread_t tid = pthread_self();
-    printf("Process %d: Thread PID: %d, TID: %lu\n", *(int*)arg, pid, tid);
+   // printf("Process %d: Thread PID: %d, TID: %lu\n", *(int*)arg, pid, tid);
     return NULL;
 }
 
@@ -15,13 +16,13 @@ int main(int argc, char** argv) {
     MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    printf("Process %d: Provided level of thread support: %d\n", rank, provided);
+    //printf("Process %d: Provided level of thread support: %d\n", rank, provided);
     
     pthread_t thread;
     pthread_create(&thread, NULL, print_pid_tid, &rank);
     pthread_join(thread, NULL);
     
-    MPI_Finalize();
+//    MPI_Finalize();
     
     return 0;
 }
--- _8_beartooth/cMPIThread/mpithread.slurm.sh
+++ _8_beartooth/cMPIThread/mpithread.slurm.sh
@@ -2,7 +2,6 @@
 
 #SBATCH --account=communitybem
 #SBATCH --time=00:01:00
-#SBATCH --ntasks=3
 #SBATCH --job-name=test_lw
 #SBATCH --output=test_lw%j.out
 #SBATCH --error=test_lw%j.err
