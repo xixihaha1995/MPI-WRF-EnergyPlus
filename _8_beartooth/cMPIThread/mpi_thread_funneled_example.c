9baf94d7ab1801c956ec709db27e9dab6f8f1947
linuxmint@optiplex-7080 <wulicheneason@gmail.com>
2023-05-28 17:27:36 -0600

    update

--- _8_beartooth/cMPIThread/mpi_thread_funneled_example.c
+++ _8_beartooth/cMPIThread/mpi_thread_funneled_example.c
@@ -13,7 +13,14 @@ void* print_pid_tid(void* arg) {
 int main(int argc, char** argv) {
     int rank, provided;
     
+    //print arguments
+    printf("argc: %d\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
     MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+    printf("provided: %d\n", provided);
+    printf("MPI_THREAD_FUNNELED: %d\n", MPI_THREAD_FUNNELED);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     printf("Process %d: Provided level of thread support: %d\n", rank, provided);
