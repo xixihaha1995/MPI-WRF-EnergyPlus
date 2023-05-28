558e588d220b22a574a6e653a95123aa03fee738
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-05-28 12:09:51 -0600

    Create wrf.slurm.sh

---
+++ _8_beartooth/bSingleDomain/wrf.slurm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account=communitybem
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=test_lw
+#SBATCH --output=test_lw%j.out
+#SBATCH --error=test_lw%j.err
+
+# Set the directory where you want to install the software, Change to the source code directory
+DIR="/project/communitybem/NWP/WRF/test/em_real"
+cd $DIR
+pwd
+
+# Execute the program
+mpirun ./wrf.exe
