1f51b5b985233f6834121dd453259cefef64a051
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-05-28 18:16:49 -0600

    Update mpi_thread_funneled_example.c

--- _8_beartooth/cMPIThread/mpi_thread_funneled_example.c
+++ _8_beartooth/cMPIThread/mpi_thread_funneled_example.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define MAX_MSG_LEN 128
+
 void* print_pid_tid(void* arg) {
     pid_t pid = getpid();
     pthread_t tid = pthread_self();
@@ -11,19 +13,21 @@ void* print_pid_tid(void* arg) {
 }
 
 int main(int argc, char** argv) {
-    int rank, provided;
-    
+    int rank, provided, size;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+
     //print arguments
     printf("argc: %d\n", argc);
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
     MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
-    printf("provided: %d\n", provided);
-    printf("MPI_THREAD_FUNNELED: %d\n", MPI_THREAD_FUNNELED);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Get_processor_name(processor_name, &size);
+    printf("Rank %d from comm %d at processor %s, size: %d\n", rank, MPI_COMM_WORLD, processor_name, size);
 
-    printf("Process %d: Provided level of thread support: %d\n", rank, provided);
+    printf("Rank %d: Provided level of thread support: %d\n", rank, provided);
     
     pthread_t thread;
     pthread_create(&thread, NULL, print_pid_tid, &rank);
