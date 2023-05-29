fc4145ef9f3175aabc8dfea0d22eb6d60c2026cf
Lichen Wu <lwu4@blog2.cluster>
2023-05-29 09:17:59 -0600

    run on my pc

--- _8_beartooth/cMPIThread/mpithread.slurm.sh
+++ _8_beartooth/cMPIThread/mpithread.slurm.sh
@@ -3,16 +3,18 @@
 #SBATCH --account=communitybem
 #SBATCH --time=00:01:00
 #SBATCH --ntasks=5
+#SBATCH --cpus-per-task=1
 #SBATCH --job-name=test_lw
 #SBATCH --output=test_lw%j.out
 #SBATCH --error=test_lw%j.err
 
 # Set the directory where you want to install the software, Change to the source code directory
 DIR="/home/lwu4/fortran_experiments/_8_beartooth/cMPIThread"
+export OMP_NUM_THREADS=1
 cd $DIR
 pwd
 make
 echo "Running on $(hostname)"
 # Execute the program
-srun ./mpi_thread_funneled_example
+srun -verbose ./mpi_thread_funneled_example
 make clean
