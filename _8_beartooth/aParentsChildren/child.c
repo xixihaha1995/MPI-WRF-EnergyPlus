7406d84b34324c559f11be7f04775da1cd141559
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-05 13:40:49 -0600

    modify the processor related info

--- _8_beartooth/aParentsChildren/child.c
+++ _8_beartooth/aParentsChildren/child.c
@@ -4,7 +4,7 @@
 #define MPI_MAX_PROCESSOR_NAME 128
 
 int main(int argc, char** argv) {
-    int rank, size, msg, namelen;
+    int rank, world_size, msg, namelen;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     MPI_Comm parent_comm;
     MPI_Status status;
@@ -12,9 +12,9 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_get_parent(&parent_comm);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Get_processor_name(processor_name, &namelen);
 
-    printf("Child/parent %d/%d: rank=%d, size=%d, name=%s\n", MPI_COMM_WORLD, parent_comm, rank, size, processor_name);
+    printf("Child: rank=%d, size=%d, name=%s\n", rank, world_size, processor_name);
     return 0;
 }
--- _8_beartooth/aParentsChildren/parent.c
+++ _8_beartooth/aParentsChildren/parent.c
@@ -14,23 +14,20 @@ int main(int argc, char** argv) {
 }
 
 void spawn_children(void) {
-    int rank, size, nbr_child = 1;
-    char* command = "/home/lwu4/fortran_experiments/_8_beartooth/aParentsChildren/child.exe";
+    int rank, world_size, nbr_child = 3, name_len;
+    char* command = "./child.exe";
     char msgstr[MAX_MSG_LEN];
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     MPI_Status status;
-    MPI_Info info;
     MPI_Comm child_comm;
 
-    MPI_Info_create(&info);
-    MPI_Info_set(info, "host", "m199");
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Get_processor_name(processor_name, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Get_processor_name(processor_name, &name_len);
 
    // MPI_Comm_spawn(command, MPI_ARGV_NULL, nbr_child, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &child_comm, MPI_ERRCODES_IGNORE);
- 
-    
-    printf("Parent %d from comm %d at processor %s\n", rank, MPI_COMM_WORLD, processor_name);
 
+    printf("Parent: Rank %d from comm %d (size %d )at processor %s\n", rank, MPI_COMM_WORLD,world_size, processor_name);
+    MPI_Finalize();
+    return 0;
 }
