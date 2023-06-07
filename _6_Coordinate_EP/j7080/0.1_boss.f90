cb0e48357f284202bcfa44e88d15149e14f0813c
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-07 11:19:20 -0600

    Update 0.1_boss.f90

--- _6_Coordinate_EP/j7080/0.1_boss.f90
+++ _6_Coordinate_EP/j7080/0.1_boss.f90
@@ -1,5 +1,5 @@
 program mpi_app
-    integer :: time_idx, timesteps = 6 * 24 + 10, allix = 3, alliy =3, curix, curiy, curibui = 1, curitime
+    integer :: time_idx, timesteps = 6 * 24 + 10, allix = 3, alliy =3, curix, curiy, curibui = 1, curitime = 1
     real(kind=8) :: random_oat_c,mean_recv_waste_j
 
     do time_idx = 1, timesteps
@@ -20,7 +20,7 @@ contains
         include 'mpif.h'
         integer :: ierr, rank, num_procs, parent_comm, child_idx, status(MPI_STATUS_SIZE), curix, curiy, curibui, curitime
         integer, save :: new_comm,  saveix, saveiy, saveibui
-        integer ::  calling = 0, num_children = 3, ending_steps = 6*20, ucm_tag = 0
+        integer ::  calling = 0, num_children = 3, ending_steps = 6*24, ucm_tag = 0
         REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: received_data
         real(kind=8) :: random_oat_c, mean_recv_waste_j
         logical :: initedMPI, spawned = .false., turnMPIon = .true.
