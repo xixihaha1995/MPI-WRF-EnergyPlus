6bef37f5b3a55f097912fe9cef263f0620267997
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-06 23:46:51 -0600

    WRF and EP
    Largest domain
    Averaged building types in one urban class.
    hourly communication(or 180 seconds, 150 seconds)
    upscaling the number of IDFs
    deploy them on HPC

---
+++ _6_Coordinate_EP/j7080/0.1_boss.f90
@@ -0,0 +1,99 @@
+program mpi_app
+    integer :: time_idx, timesteps = 6 * 24 + 10, allix = 3, alliy =3, curix, curiy
+    real(kind=8) :: random_oat_c,mean_recv_waste_j
+
+    do time_idx = 1, timesteps
+        do curiy = 1, alliy
+            do curix = 1, allix
+                call random_number(random_oat_c)
+                random_oat_c = 12 + int(random_oat_c*28)
+                call spawn_children(curix,curiy,random_oat_c,mean_recv_waste_j)
+                print * , "mean_recv_waste_j", mean_recv_waste_j
+            end do
+        end do
+    end do
+
+contains
+
+    subroutine spawn_children(curix,curiy,random_oat_c,mean_recv_waste_j)
+        implicit none
+        include 'mpif.h'
+        integer :: ierr, rank, num_procs, parent_comm, child_idx, status(MPI_STATUS_SIZE), curix, curiy
+        integer, save :: new_comm,  saveix, saveiy
+        integer ::  calling = 0, num_children = 3, ending_steps = 6*24, ucm_tag = 0
+        REAL(KIND=8), DIMENSION(:), ALLOCATABLE :: received_data
+        real(kind=8) :: random_oat_c, mean_recv_waste_j
+        logical :: initedMPI, spawned = .false., turnMPIon = .false.
+        character(len=50) :: command
+        ALLOCATE (received_data(num_children))
+
+        if (calling == 0) then
+            saveix = curix
+            saveiy = curiy
+        end if
+
+        !if curix and curiy are not the same as saveix and saveiy, then return
+        if (curix /= saveix .or. curiy /= saveiy) then
+            !print *, "curix /= saveix .or. curiy /= saveiy, returning"
+            return
+        end if
+
+
+        calling = calling + 1
+        print *, "calling spawn_children() counts:", calling, "curix", curix, "curiy", curiy
+
+        if (turnMPIon .eqv. .false.) then
+            !print *, "turnMPIon is false, no more MPI calls"
+            return
+        end if
+
+        if (spawned .eqv. .false.) then
+            spawned = .true.
+            call MPI_Initialized(initedMPI, ierr)
+            if (.not. initedMPI) then
+                print *, "MPI not initialized, initializing"
+                call MPI_Init(ierr)
+            end if
+
+            call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+            call MPI_Comm_size(MPI_COMM_WORLD, num_procs, ierr)
+            !print *, "MPI_COMM_WORLD", MPI_COMM_WORLD, "num_procs", num_procs, "rank", rank
+
+            call MPI_Comm_dup(MPI_COMM_WORLD, parent_comm, ierr)
+            command = "./child.exe"
+            call MPI_Comm_spawn(command, MPI_ARGV_NULL, num_children, MPI_INFO_NULL, &
+                    0, parent_comm, new_comm, MPI_ERRCODES_IGNORE, ierr)
+            !print *, "Parent comm: ", parent_comm, " new comm: ", new_comm
+        end if
+
+        if (calling > ending_steps - 1) then
+            ucm_tag = 886
+            turnMPIon = .false.
+        else
+            ucm_tag = 0
+        end if
+
+        do child_idx = 1, num_children
+!            call random_number(random_oat_c)
+!            random_oat_c = 12 + int(random_oat_c*28)
+            !            print *, "new_comm:", new_comm
+            !            new_comm = -2080374783
+            call MPI_Sendrecv(random_oat_c, 1, MPI_REAL8, child_idx - 1, ucm_tag, &
+                    received_data(child_idx), 1, MPI_REAL8, child_idx - 1, MPI_ANY_TAG, new_comm, status, ierr)
+        end do
+        mean_recv_waste_j = sum(received_data)/num_children
+        !print *, "WRF (Parent(s)) received_data (waste heat J)", received_data, "mean_recv_waste_j", mean_recv_waste_j
+
+
+        if (ucm_tag == 886) then
+!            print *, "WRF (Parent(s)) ending messsage 886 sent, &
+!                    &to reach collective barrier,(no more inter-communicator calls)&
+!                    & only WRF global setting call free and MPI_Finalize()"
+            call MPI_Barrier(new_comm, ierr)
+            !            call MPI_Comm_free(new_comm, ierr)
+            !            call MPI_Comm_free(parent_comm, ierr)
+            !            call MPI_Finalize(ierr)
+        end if
+    end subroutine spawn_children
+
+end program mpi_app
---
+++ _6_Coordinate_EP/j7080/README.md
@@ -0,0 +1,6 @@
+### Average different type of buildings in an urban class
+https://github.com/wrf-model/WRF/blob/21c72141142fc6c8d203d2bf79f1990e45a0aef8/phys/module_sf_bep_bem.F#L1470
+https://github.com/wrf-model/WRF/blob/21c72141142fc6c8d203d2bf79f1990e45a0aef8/phys/module_sf_bep_bem.F#L2214
+https://github.com/wrf-model/WRF/blob/21c72141142fc6c8d203d2bf79f1990e45a0aef8/phys/module_sf_bep_bem.F#L1552
+https://github.com/wrf-model/WRF/blob/21c72141142fc6c8d203d2bf79f1990e45a0aef8/phys/module_sf_bep_bem.F#L5336
+https://github.com/NCAR/noahmp/blob/3be0b2860dab167006a0b3c4822e234ca253c3df/drivers/wrf/module_sf_noahmpdrv.F#L3737
