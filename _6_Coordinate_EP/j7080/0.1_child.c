5817a7693ec11caac9b8043efa9dc9570038bd6b
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-08 15:30:50 -0600

    Update 0.1_child.c

--- _6_Coordinate_EP/j7080/0.1_child.c
+++ _6_Coordinate_EP/j7080/0.1_child.c
@@ -131,7 +131,7 @@ int main(int argc, char** argv) {
     requestVariable(state, "HVAC System Total Heat Rejection Energy", "SIMHVAC");
 
     sprintf(output_path, "./ep_trivial_%d", rank);
-    sprintf(idfFilePath, "./resources/in_uwyo_1.idf");
+    sprintf(idfFilePath, "../../_9_urbanopt/resources-22-2-0/in_uwyo_%d.idf", rank);
 
     char* weather_file_path = "./resources/USA_NY_Buffalo-Greater.Buffalo.Intl.AP.725280_TMY3.epw";
     const char* sys_args[] = {"-d", output_path, "-w", weather_file_path, idfFilePath, NULL};
