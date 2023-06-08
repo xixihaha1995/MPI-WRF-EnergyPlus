21fc02a480517fb1117881fcae814398703b0916
linuxmint@optiplex-7080 <wulicheneason@gmail.com>
2023-06-08 14:06:53 -0600

    dummy

--- _6_Coordinate_EP/j7080/update.sh
+++ _6_Coordinate_EP/j7080/update.sh
@@ -3,6 +3,7 @@
 # Step 1: Copy a to b
 cp saved.module_sf_bep_bem.F /home/xxx/NWP/WRF/phys/module_sf_bep_bem.F 
 cp -r resources /home/xxx/NWP/WRF/test/em_real/
+make
 cp child.exe /home/xxx/NWP/WRF/test/em_real/child.exe
 
 # Step 3: Git add
