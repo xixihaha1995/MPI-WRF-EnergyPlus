405c1243f3a77dfc5eae1b399476f66f2ddc6c7b
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-24 17:49:55 -0600

    Update 0.1_child.c

--- _6_Coordinate_EP/j7080/0.1_child.c
+++ _6_Coordinate_EP/j7080/0.1_child.c
@@ -229,7 +229,8 @@ void endSysTimeStepHandler(EnergyPlusState state) {
     }
 
     float data[performanc_length];
-    data[0] = uwyo1.footPrintM2;
+    // data[0] = uwyo1.footPrintM2;
+    data[0] = -66.0;
     data[1] = (float) simHVAC_W;
     // bot 4, mid 4, top 4
     for (int i = 0; i < 4; i++) {
