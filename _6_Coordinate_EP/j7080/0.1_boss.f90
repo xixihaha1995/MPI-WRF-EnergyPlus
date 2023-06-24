5bfd8f66679a6a24bd231b29e624e200cf8bdcb6
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-24 17:45:34 -0600

    use child to control online or offline

--- _6_Coordinate_EP/j7080/0.1_boss.f90
+++ _6_Coordinate_EP/j7080/0.1_boss.f90
@@ -19,6 +19,9 @@ program mpi_app
                     random_weather(2) = 0.007612 + int(random_data*0.0001)
                     random_weather(3) = 101325 + int(random_data*1000)
                     call spawn_children(curix,curiy,curibui,dt,time_idx,xlat, xlong, random_weather,wM2_12K)
+                    if (wM2_12K(1) .eqv. -66) then 
+                        continue
+                    end if
                     wrfWaste(1:7) = wM2_12K(1)
                     do i = 1, 3
                         wrfSurface(:,10, i) = wM2_12K(2+(i-1)*4:5+(i-1)*4)
@@ -80,7 +83,7 @@ contains
       ! print *, "curibui", curibui, "curitime", curitime
      
       !if calling is % 540, for 6.667s per step; 540 steps for one hour, then carry on, otherwise return
-      if (mod(calling,540) /= 0 .or. hourlyUpdate .eqv. .true.) then
+      if (calling .eqv. 1 .or. mod(calling,540) /= 0 .or. hourlyUpdate .eqv. .true.) then
             ! Forward filling for any time steps, any building types
             ! print *, "Forward filling curitime", curitime, "curibui", curibui, "wM2_12K", wM2_12K
             return
