81c14675558e2bc6a7a1598ffb3cb51859ec6b02
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-24 15:31:18 -0600

    Update 0.1_boss.f90

--- _6_Coordinate_EP/j7080/0.1_boss.f90
+++ _6_Coordinate_EP/j7080/0.1_boss.f90
@@ -6,6 +6,7 @@ program mpi_app
     real, dimension(13) :: wM2_12K;
     real, dimension(18) :: wrfWaste;
     real, dimension(4,10,18) :: wrfSurface;
+    real, dimension(4,1,3) :: reshapeSurface;
     real :: dt = 6.67, xlat = 41.30, xlong = -105.59
 
 
@@ -19,7 +20,8 @@ program mpi_app
                     random_weather(3) = 101325 + int(random_data*1000)
                     call spawn_children(curix,curiy,curibui,dt,time_idx,xlat, xlong, random_weather,wM2_12K)
                     wrfWaste(1:7) = wM2_12K(1)
-                    wrfSurface(:,10, 1:3) = wM2_12K(2:14)
+                    reshapeSurface = reshape(wrfSurface, (/4,1,3/))
+                    wrfSurface(:,10, 1:3) = reshapeSurface
 
                 end do
             end do
