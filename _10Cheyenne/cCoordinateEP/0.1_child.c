59769600164e17437f7cb74f1ebd74a04524550a
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-30 22:59:51 -0600

    Update 0.1_child.c

--- _10Cheyenne/cCoordinateEP/0.1_child.c
+++ _10Cheyenne/cCoordinateEP/0.1_child.c
@@ -10,13 +10,125 @@
 
 #define MPI_MAX_PROCESSOR_NAME 128
 
+typedef struct {
+    float footPrintM2;
+    int bot[4];
+    int* mid;
+    int top[4];
+} GeoUWyo;
+
+typedef struct {
+    int botHandle[4];
+    int* midHandle;
+    int topHandle[4];
+} SurfaceHandles;
+
+typedef struct {
+    Real64 botVal[4];
+    Real64* midVal;
+    Real64 topVal[4];
+} SurfaceValues;
+
 int handlesRetrieved = 0, weatherHandleRetrieved = 0;
 int simHVACSensor = 0, odbActHandle = 0, orhActHandle = 0, odbSenHandle = 0, ohrSenHandle = 0;
-int rank = -1;
-Real64 msg_arr[3] = {-1, -1, -1};
+int rank = -1, performanc_length =2;
+float msg_arr[3] = {-1, -1, -1};
+float footprintm2[38] = {
+    162.15, 2513.40, 37.75, 355.15, 1049.87, 415.98,
+    2608.08, 115.65, 1793.84, 2785.14,958.38,2745.55,
+    1292.35,347.83,2464.88,9048.44,548.43,185.96,
+    828.75,91.38,107.44,90.02,181.64,1698.19,
+    4090.41,24400.54,158.22,4229.28,92.32,108.30,
+    3781.44,123.20,3298.50,257.05,841.63,80.27,
+    1808.91,889.49
+};
 int weatherMPIon = 1, wasteMPIon = 1;
+int isOnline = 0;
 MPI_Comm parent_comm;
 MPI_Status status;
+SurfaceHandles surHandles;
+SurfaceValues surValues;
+
+
+
+int midNames[] = {38, 50, 56, 44, 68, 80, 86, 74};
+int midLen = sizeof(midNames) / sizeof(midNames[0]);
+Real64* tempMidVal;
+
+GeoUWyo uwyo1 = {
+    .footPrintM2 = 162.15,
+    .bot = {8, 20, 26, 14},
+    .mid = midNames,
+    .top = {98, 110, 116, 104}
+};
+
+
+
+// I'd like add three more functions related with GeoUWyo1 surfaces,
+// one is used to request the surface variables
+// one is used to get surface handle, and check if it is valid
+// one is used to get surface variable value.
+
+void requestSur(EnergyPlusState state, GeoUWyo geoUWyo) {
+    // char surfaceName[100];
+    // sprintf(surfaceName, "Surface %d", geoUWyo.bot[0]);
+    // requestVariable(state, "Surface Outside Face Temperature", surfaceName);
+    // This function is used to iterate bot, mid, top surfaces. request them
+
+    char surfaceName[100];
+    for (int i = 0; i < 4; i++) {
+        sprintf(surfaceName, "Surface %d", geoUWyo.bot[i]);
+        requestVariable(state, "Surface Outside Face Temperature", surfaceName);
+        sprintf(surfaceName, "Surface %d", geoUWyo.top[i]);
+        requestVariable(state, "Surface Outside Face Temperature", surfaceName);
+    }
+    for (int i = 0; i < midLen; i++) {
+        sprintf(surfaceName, "Surface %d", geoUWyo.mid[i]);
+        requestVariable(state, "Surface Outside Face Temperature", surfaceName);
+    }
+}
+
+SurfaceHandles getSurHandle(EnergyPlusState state, GeoUWyo geoUWyo) {
+    // This function is used to iterate bot, mid, top surfaces. get their handles
+    SurfaceHandles surHandles;
+    char surfaceName[100];
+    for (int i = 0; i < 4; i++) {
+        sprintf(surfaceName, "Surface %d", geoUWyo.bot[i]);
+        surHandles.botHandle[i] = getVariableHandle(state, "Surface Outside Face Temperature", surfaceName);
+        sprintf(surfaceName, "Surface %d", geoUWyo.top[i]);
+        surHandles.topHandle[i] = getVariableHandle(state, "Surface Outside Face Temperature", surfaceName);
+        if (surHandles.botHandle[i] < 0 || surHandles.topHandle[i] < 0) {
+            printf("Error: surHandles.botHandle[%d] = %d, surHandles.topHandle[%d] = %d\n",
+                   i, surHandles.botHandle[i], i, surHandles.topHandle[i]);
+            exit(1);
+        }
+    }
+    for (int i = 0; i < midLen; i++) {
+        sprintf(surfaceName, "Surface %d", geoUWyo.mid[i]);
+        surHandles.midHandle[i] = getVariableHandle(state, "Surface Outside Face Temperature", surfaceName);
+        if (surHandles.midHandle[i] < 0) {
+            printf("Error: surHandles.midHandle[%d] = %d\n", i, surHandles.midHandle[i]);
+            exit(1);
+        }
+    }
+    return surHandles;
+}
+
+SurfaceValues getSurVal(EnergyPlusState state, SurfaceHandles surHandles) {
+    // This function is used to iterate bot, mid, top surfaces. get their values
+    SurfaceValues surValues;
+    for (int i = 0; i < 4; i++) {
+        surValues.botVal[i] = getVariableValue(state, surHandles.botHandle[i]);
+        surValues.topVal[i] = getVariableValue(state, surHandles.topHandle[i]);
+    }
+    tempMidVal = malloc(midLen * sizeof(Real64));
+    for (int i = 0; i < midLen; i++) {
+        Real64 midVal = getVariableValue(state, surHandles.midHandle[i]);
+        tempMidVal[i] = midVal;
+    }
+    surValues.midVal = tempMidVal;
+    return surValues;
+}
 
 void overwriteEpWeather(EnergyPlusState state) {
     if (weatherHandleRetrieved == 0) {
@@ -29,6 +141,7 @@ void overwriteEpWeather(EnergyPlusState state) {
         orhActHandle = getActuatorHandle(state, "Weather Data", "Outdoor Relative Humidity", "ENVIRONMENT");
         odbSenHandle = getVariableHandle(state, "SITE OUTDOOR AIR DRYBULB TEMPERATURE", "ENVIRONMENT");
         ohrSenHandle = getVariableHandle(state, "Site Outdoor Air Humidity Ratio", "ENVIRONMENT");
+
 
         if (odbActHandle < 0 || orhActHandle < 0 || odbSenHandle < 0 || ohrSenHandle < 0)
         {
@@ -53,11 +166,12 @@ void overwriteEpWeather(EnergyPlusState state) {
         return;
     }
     // MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Recv(&msg_arr, 3, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, parent_comm, &status);
+    MPI_Recv(&msg_arr, 3, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, parent_comm, &status);
     if (status.MPI_TAG == 886)
     {
         printf("EnergyPlus(BEMs):%d received 'ending messsage 886', "
-               "to reach collective barrier, only WRF call free MPI_Finalize().\n", rank);
+               "to reach collective barrier, EP wait WRF to finalize, then EP free MPI_Finalize().\n"
+               "Then no more MPI at all on both sides.", rank);
         weatherMPIon = 0;
     }
     Real64 rh = 100 * psyRhFnTdbWPb(state, msg_arr[0], msg_arr[1], msg_arr[2]);
@@ -81,6 +195,8 @@ void endSysTimeStepHandler(EnergyPlusState state) {
         }
         handlesRetrieved = 1;
         simHVACSensor = getVariableHandle(state, "HVAC System Total Heat Rejection Energy", "SIMHVAC");
+        // surHandles = getSurHandle(state, uwyo1);
+        
         if (simHVACSensor < 0)
         {
             printf("Error: simHVACSensor = %d\n", simHVACSensor);
@@ -98,24 +214,54 @@ void endSysTimeStepHandler(EnergyPlusState state) {
     }
     Real64 simTimeInHours = currentSimTime(state);
     Real64 simTime = simTimeInHours * 3600;
-    Real64 simHVAC = getVariableValue(state, simHVACSensor);
+    Real64 simHVAC_J = getVariableValue(state, simHVACSensor);
+    Real64 simHVAC_W = simHVAC_J/ 3600;
 
+    // surValues = getSurVal(state, surHandles);
+    // // for surValues.midVal, its length is a multiple of 4. Average it into 4 values
+    // Real64 avgMidVal[4];
+    // for (int i = 0; i < midLen; i++) {
+    //     avgMidVal[i % 4] += surValues.midVal[i];
+    // }
+    // for (int i = 0; i < 4; i++) {
+    //     int num = midLen / 4;
+    //     avgMidVal[i] /= num;
+    //     // printf("Botom surface %d temperature = %.2f (C)\n", i, surValues.botVal[i]);
+    //     // printf("Mid surface %d temperature = %.2f (C)\n", i, avgMidVal[i]);
+    //     // printf("Top surface %d temperature = %.2f (C)\n", i, surValues.topVal[i]);
+    // }
+
+    // free(tempMidVal);
     if (! wasteMPIon)
     {
-        printf("Child rank = %d wasteMPIon=0, No more MPI, simTime = %.2f (s), simHVAC = %.2f (J), \n", rank, simTime, simHVAC);
+        printf("Child rank = %d wasteMPIon=0, No more MPI, simTime = %.2f (s), simHVAC_W = %.2f (W), \n", rank, simTime, simHVAC_W);
         return;
     }
 
-    MPI_Send(&simHVAC, 1, MPI_DOUBLE, status.MPI_SOURCE, 0, parent_comm);
-    printf("Child %d sent heat %.2f (J) to it, at time %.2f(s)\n",
-           rank,simHVAC, simTime);
+    float data[performanc_length];
+    data[rank] = footprintm2[rank];
+    if (isOnline)
+        data[1] = (float) simHVAC_W;
+    else
+        data[1] = -66.0;
+    // bot 4, mid 4, top 4
+    // for (int i = 0; i < 4; i++) {
+    //     data[i + 2] = (float) (surValues.botVal[i] + 273.15);
+    //     data[i + 6] = (float) (avgMidVal[i] + 273.15);
+    //     data[i + 10] = (float) (surValues.topVal[i] + 273.15);
+    // }
+
+    MPI_Send(&data, performanc_length, MPI_FLOAT,status.MPI_SOURCE, 0, parent_comm);
+    printf("Child %d sent heat %.2f (W) to it, at time %.2f(s)\n",
+           rank,simHVAC_W, simTime);
     
     if (!weatherMPIon) {
         printf("Child %d reached collective barrier, all my siblings here, let's end MPI. \n", rank);
         MPI_Barrier(parent_comm);
         wasteMPIon = 0;
         // sleep 5 seconds
-        usleep(20000000);
+        int sleeptimeP8S = 800000;
+        usleep(sleeptimeP8S*100);
         MPI_Finalize();
     }
     
@@ -142,11 +288,24 @@ int main(int argc, char** argv) {
     requestVariable(state, "Site Outdoor Air Drybulb Temperature", "ENVIRONMENT");
     requestVariable(state, "Site Outdoor Air Humidity Ratio", "ENVIRONMENT");
     requestVariable(state, "HVAC System Total Heat Rejection Energy", "SIMHVAC");
-
-    sprintf(output_path, "/glade/scratch/lichenwu/ep_temp/ep_trivial_%d", rank+1);
+    // requestSur(state, uwyo1);
+    char curpath[256];
+    getcwd(curpath, sizeof(curpath));
+    if (strstr(curpath, "glade")) {
+        if (isOnline) 
+             sprintf(output_path, "/glade/scratch/lichenwu/ep_temp/saved_online_ep_trivial_%d", rank + 1);
+        else
+            sprintf(output_path, "/glade/scratch/lichenwu/ep_temp/saved_offline_ep_trivial_%d", rank + 1);
+    } else {
+        if (isOnline) 
+            sprintf(output_path, "./saved_online_ep_trivial_%d", rank + 1);
+        else
+            sprintf(output_path, "./saved_offline_ep_trivial_%d", rank + 1);
+    }
+    
     sprintf(idfFilePath, "./resources-23-1-0/in_uwyo_%d.idf", rank+1);
 
-    char* weather_file_path = "./resources-23-1-0/USA_NY_Buffalo-Greater.Buffalo.Intl.AP.725280_TMY3.epw";
+    char* weather_file_path = "./resources-23-1-0/USA_WY_Laramie-General.Brees.Field.725645_TMY3.epw";
     const char* sys_args[] = {"-d", output_path, "-w", weather_file_path, idfFilePath, NULL};
     int argc_ = sizeof(sys_args) / sizeof(char*) - 1;
     printf("argc_ = %d\n", argc_);
@@ -154,6 +313,7 @@ int main(int argc, char** argv) {
         printf("sys_args[%d] = %s\n", i, sys_args[i]);
     }
     energyplus(state,argc_, sys_args);
+ 
 
     return 0;
 }
