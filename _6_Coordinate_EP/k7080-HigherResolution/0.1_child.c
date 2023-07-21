91f8da4ce3a03aa8ff32dc1d854d827cd33aef93
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-07-21 11:14:57 +0800

    large number calculation
    how to make data consistent transferred between WRF and EP

--- _6_Coordinate_EP/k7080-HigherResolution/0.1_child.c
+++ _6_Coordinate_EP/k7080-HigherResolution/0.1_child.c
@@ -262,7 +262,7 @@ void endSysTimeStepHandler(EnergyPlusState state) {
     // }
 
     MPI_Send(&data, performanc_length, MPI_FLOAT,status.MPI_SOURCE, 0, parent_comm);
-    printf("Child %d sent flootaream2 = %.2f (m2), simHVAC_W = %.2f (W)",
+    printf("Child %d sent flootaream2 = %.2f (m2), simHVAC_W = %.2f (W)\n",
            rank, data[0], data[1]);
     
     if (!weatherMPIon) {
