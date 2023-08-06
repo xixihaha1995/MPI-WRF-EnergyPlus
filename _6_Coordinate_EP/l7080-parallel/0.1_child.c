2ae3aa5fbc53c2b86ad89120e95e6a06cc1bed0d
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-08-06 22:48:51 +0800

    Update 0.1_child.c

--- _6_Coordinate_EP/l7080-parallel/0.1_child.c
+++ _6_Coordinate_EP/l7080-parallel/0.1_child.c
@@ -318,6 +318,7 @@ int closetGridIndex(float bldlat, float bldlong){
         for (int i = 0; i < allDomainLen[j]; i++) {
             // double dist = (bldlat - latall[j][i]) * (bldlat - latall[j][i]) + (bldlong - longall[j][i]) * (bldlong - longall[j][i]);
             double dist = distanceBetweenPoints((double) bldlat, (double) bldlong, (double) latall[j][i], (double) longall[j][i]);
+            printf("dist = %.2f, minDist = %.2f\n", dist, minDist);
             if (dist < minDist) {
                 minDist = dist;
                 minIndex = i;
