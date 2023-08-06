37aca65f97af91a558a71d1655ac609d6da95c48
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-08-06 09:17:36 +0800

    Update 0.1_child.c

--- _6_Coordinate_EP/l7080-parallel/0.1_child.c
+++ _6_Coordinate_EP/l7080-parallel/0.1_child.c
@@ -359,7 +359,7 @@ void receiveLongLat(void) {
         MPI_Send(&IDF_Coupling, 1, MPI_INT, j, 0, parent_comm);
         MPI_Send(&mappings[j], INNERMOST_POINTS * INNERMOST_POINTS * NBR_IDF, MPI_INT, j, 0, parent_comm);
         
-        // printf("Child %d sent mappings to WRF%d\n", rank, j);
+        printf("Child %d sent mappings to WRF%d\n", rank, j);
     }
 
     // for (int i = 0; i < INNERMOST_POINTS * INNERMOST_POINTS; i++) {
