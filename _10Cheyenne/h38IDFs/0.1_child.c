75cb9f7b8948bb84dc51b91c37390f2b9362ca5a
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-08-30 20:48:18 +0800

    test offline

--- _10Cheyenne/h38IDFs/0.1_child.c
+++ _10Cheyenne/h38IDFs/0.1_child.c
@@ -20,7 +20,7 @@
 #define MAPPING_TAG 6
 #define EARTH_RADIUS_KM 6371.0
 
-int IDF_Coupling = 2; //0, offline; 1, waste; 2, waste + surface;
+int IDF_Coupling = 0; //0, offline; 1, waste; 2, waste + surface;
 
 typedef struct {
     int gridIdx;
