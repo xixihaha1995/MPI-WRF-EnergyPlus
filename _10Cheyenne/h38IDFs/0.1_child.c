516f22e805423b14791a3476e099b27e0d7ac549
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-08-31 11:02:16 +0800

    then offline

--- _10Cheyenne/h38IDFs/0.1_child.c
+++ _10Cheyenne/h38IDFs/0.1_child.c
@@ -20,7 +20,7 @@
 #define MAPPING_TAG 6
 #define EARTH_RADIUS_KM 6371.0
 
-int IDF_Coupling = 2; //0, offline; 1, waste; 2, waste + surface;
+int IDF_Coupling = 0; //0, offline; 1, waste; 2, waste + surface;
 float date_numer = 7.1;
 
 typedef struct {
